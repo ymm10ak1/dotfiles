130b063a9273cd4920f1995c89dfba5244e0a479
ymm10 <rihanito30@gmail.com>
2024-05-06 19:08:30 +0900

    [Update] 各スクリプトに対してshfmtを適用

--- install.sh
+++ install.sh
@@ -8,73 +8,73 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd -P)
 BACKUP_DATE=$(date "+%Y%m%d-%H%M%S")
 
 create_symlink_dotfiles() {
-	command echo "$BACKUP_DATE backup old dotfiles..."
-	if [ ! -d "$HOME/.dotbackup/$BACKUP_DATE" ]; then
-		command echo "$HOME/.dotbackup/$BACKUP_DATE not found. Auto Make it"
-		command mkdir -pv "$HOME/.dotbackup/$BACKUP_DATE"
-	fi
+  command echo "$BACKUP_DATE backup old dotfiles..."
+  if [ ! -d "$HOME/.dotbackup/$BACKUP_DATE" ]; then
+    command echo "$HOME/.dotbackup/$BACKUP_DATE not found. Auto Make it"
+    command mkdir -pv "$HOME/.dotbackup/$BACKUP_DATE"
+  fi
 
-	local dot_dir="${SCRIPT_DIR}"
-	local backup_dir="$HOME/.dotbackup/$BACKUP_DATE"
-	if [[ "$HOME" != "$dot_dir" ]]; then
-		for file in $dot_dir/.??*; do
-			[[ $(basename $file) == ".git" ]] && continue
-			[[ $(basename $file) == ".gitignore" ]] && continue
-			[[ $(basename $file) == ".config" ]] && continue
-			[[ $(basename $file) == "scripts" ]] && continue
-			if [[ -L "$HOME/$(basename $file)" ]]; then
-				command rm -f "$HOME/$(basename $file)"
-			fi
-			if [[ -e "$HOME/$(basename $file)" && ! -L "$HOME/$(basename $file)" ]]; then
-				command mv "$HOME/$(basename $file)" "$backup_dir"
-			fi
-			command ln -snfv $file $HOME
-		done
-	else
-		command echo "same install src dest"
-	fi
+  local dot_dir="${SCRIPT_DIR}"
+  local backup_dir="$HOME/.dotbackup/$BACKUP_DATE"
+  if [[ "$HOME" != "$dot_dir" ]]; then
+    for file in $dot_dir/.??*; do
+      [[ $(basename $file) == ".git" ]] && continue
+      [[ $(basename $file) == ".gitignore" ]] && continue
+      [[ $(basename $file) == ".config" ]] && continue
+      [[ $(basename $file) == "scripts" ]] && continue
+      if [[ -L "$HOME/$(basename $file)" ]]; then
+        command rm -f "$HOME/$(basename $file)"
+      fi
+      if [[ -e "$HOME/$(basename $file)" && ! -L "$HOME/$(basename $file)" ]]; then
+        command mv "$HOME/$(basename $file)" "$backup_dir"
+      fi
+      command ln -snfv $file $HOME
+    done
+  else
+    command echo "same install src dest"
+  fi
 }
 
 create_symlink_config() {
-	command echo "backup old dotconfig..."
-	if [ ! -d "$HOME/.dotbackup/$BACKUP_DATE/.config" ]; then
-		command echo "$HOME/.dotbackup/$BACKUP_DATE/.config not found. Auto Make it"
-		command mkdir -pv "$HOME/.dotbackup/$BACKUP_DATE/.config"
-	fi
+  command echo "backup old dotconfig..."
+  if [ ! -d "$HOME/.dotbackup/$BACKUP_DATE/.config" ]; then
+    command echo "$HOME/.dotbackup/$BACKUP_DATE/.config not found. Auto Make it"
+    command mkdir -pv "$HOME/.dotbackup/$BACKUP_DATE/.config"
+  fi
 
-	if [ ! -d "$HOME/.config" ]; then
-		command echo "$HOME/.config not found. Auto Make it"
-		command mkdir -v "$HOME/.config"
-	fi
+  if [ ! -d "$HOME/.config" ]; then
+    command echo "$HOME/.config not found. Auto Make it"
+    command mkdir -v "$HOME/.config"
+  fi
 
-	local config_dir="${SCRIPT_DIR}/.config"
-	local home_config="$HOME/.config"
-	local backup_config_dir="$HOME/.dotbackup/$BACKUP_DATE/.config"
-	if [[ "$HOME" != "$config_dir" ]]; then
-		for file in $config_dir/??*; do
-			if [ -d $file ]; then
-				[[ $(basename $file) = ".git" ]] && continue
-				[[ $(basename $file) = "wezterm" ]] && continue
-				if [[ -L "$home_config/$(basename $file)" ]]; then
-					command rm -f "$home_config/$(basename $file)"
-				fi
-				if [[ -e "$home_config/$(basename $file)" && ! -L "$home_config/$(basename $file)" ]]; then
-					command mv "$home_config/$(basename $file)" "$backup_config_dir"
-				fi
-				command ln -snfv $file $home_config
-			fi
-		done
-	else
-		command echo "same install src dest"
-	fi
+  local config_dir="${SCRIPT_DIR}/.config"
+  local home_config="$HOME/.config"
+  local backup_config_dir="$HOME/.dotbackup/$BACKUP_DATE/.config"
+  if [[ "$HOME" != "$config_dir" ]]; then
+    for file in $config_dir/??*; do
+      if [ -d $file ]; then
+        [[ $(basename $file) = ".git" ]] && continue
+        [[ $(basename $file) = "wezterm" ]] && continue
+        if [[ -L "$home_config/$(basename $file)" ]]; then
+          command rm -f "$home_config/$(basename $file)"
+        fi
+        if [[ -e "$home_config/$(basename $file)" && ! -L "$home_config/$(basename $file)" ]]; then
+          command mv "$home_config/$(basename $file)" "$backup_config_dir"
+        fi
+        command ln -snfv $file $home_config
+      fi
+    done
+  else
+    command echo "same install src dest"
+  fi
 }
 
 run_script() {
-	if [[ -f "$1" ]]; then
-		source "$1"
-	else
-		echo "$1 is not exist"
-	fi
+  if [[ -f "$1" ]]; then
+    source "$1"
+  else
+    echo "$1 is not exist"
+  fi
 }
 
 command echo -e "\033[1;36mInstalling packages...\033[0m"
--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -5,80 +5,80 @@ set -eu
 INSTALL_LIST=(git curl exa build-essential xsel bat ripgrep)
 
 has() {
-	type -p "$1" >/dev/null 2>&1
+  type -p "$1" >/dev/null 2>&1
 }
 
 packages_install() {
-	# Ubuntu(WSL2)用セットアップ
-	if [[ -e /etc/debian_version ]]; then
-		if [[ -e /etc/lsb-release ]]; then
-			sudo apt-get update -y
-			sudo apt-get upgrade -y
+  # Ubuntu(WSL2)用セットアップ
+  if [[ -e /etc/debian_version ]]; then
+    if [[ -e /etc/lsb-release ]]; then
+      sudo apt-get update -y
+      sudo apt-get upgrade -y
 
-			# git,curl,exa,xsel,build-essential,bat,ripgrepのインストール
-			for pkg in "${INSTALL_LIST[@]}"; do
-				if [[ "$pkg" = "ripgrep" ]]; then
-					pkg="rg"
-				elif [[ "$pkg" = "build-essential" ]]; then
-					pkg="gcc"
-				elif [[ "$pkg" = "bat" ]]; then
-					pkg="batcat"
-				fi
-				if ! (has $pkg); then
-					sudo apt-get install -y $pkg
-				else
-					echo "$pkg is already installed"
-				fi
-			done
+      # git,curl,exa,xsel,build-essential,bat,ripgrepのインストール
+      for pkg in "${INSTALL_LIST[@]}"; do
+        if [[ "$pkg" = "ripgrep" ]]; then
+          pkg="rg"
+        elif [[ "$pkg" = "build-essential" ]]; then
+          pkg="gcc"
+        elif [[ "$pkg" = "bat" ]]; then
+          pkg="batcat"
+        fi
+        if ! (has $pkg); then
+          sudo apt-get install -y $pkg
+        else
+          echo "$pkg is already installed"
+        fi
+      done
 
-			# nvm,nodejsのインストール
-			if ! (command -v nvm >/dev/null); then
-				echo "nvm is already installed"
-			else
-				echo "Install nvm"
-				curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
-				# nvmがインストールできたらnodejsのインストール
-				if ! (command -v nvm >/dev/null); then
-					nvm install --lts
-				fi
-			fi
-			#denoのインストール
-			if ! (has "deno"); then
-				echo "Install deno"
-				curl -fsSL https://deno.land/install.sh | sh
-			else
-				echo "deno is already installed"
-			fi
-			# fzfのインストール(aptだとバージョンが古いのでgitを使う)
-			if ! (has "fzf"); then
-				echo "Install fzf"
-				local fzf_dir="$HOME/.fzf"
-				git clone --depth 1 https://github.com/junegunn/fzf.git "$fzf_dir"
-				"$fzf_dir"/install
-			else
-				echo "fzf is already installed"
-			fi
-			# zoxideのインストール
-			if ! (has "zoxide"); then
-				echo "Install zoxide"
-				curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
-				local zshrc_dir="$HOME/.zshrc"
-				if ! (command -v zsh >/dev/null); then
-					if [ -e "$zshrc_dir" ]; then
-						echo 'eval "$(zoxide init --cmd cd zsh)"' >>$zshrc_dir
-					fi
-				fi
-				local bashrc_dir="$HOME/.bashrc"
-				if ! (command -v bash >/dev/null); then
-					if [ -e "$bashrc_dir" ]; then
-						echo 'eval "$(zoxide init --cmd cd bash)"' >>$bashrc_dir
-					fi
-				fi
-			else
-				echo "zoxide is already installed"
-			fi
-		fi
-	fi
+      # nvm,nodejsのインストール
+      if ! (command -v nvm >/dev/null); then
+        echo "nvm is already installed"
+      else
+        echo "Install nvm"
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+        # nvmがインストールできたらnodejsのインストール
+        if ! (command -v nvm >/dev/null); then
+          nvm install --lts
+        fi
+      fi
+      #denoのインストール
+      if ! (has "deno"); then
+        echo "Install deno"
+        curl -fsSL https://deno.land/install.sh | sh
+      else
+        echo "deno is already installed"
+      fi
+      # fzfのインストール(aptだとバージョンが古いのでgitを使う)
+      if ! (has "fzf"); then
+        echo "Install fzf"
+        local fzf_dir="$HOME/.fzf"
+        git clone --depth 1 https://github.com/junegunn/fzf.git "$fzf_dir"
+        "$fzf_dir"/install
+      else
+        echo "fzf is already installed"
+      fi
+      # zoxideのインストール
+      if ! (has "zoxide"); then
+        echo "Install zoxide"
+        curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+        local zshrc_dir="$HOME/.zshrc"
+        if ! (command -v zsh >/dev/null); then
+          if [ -e "$zshrc_dir" ]; then
+            echo 'eval "$(zoxide init --cmd cd zsh)"' >>$zshrc_dir
+          fi
+        fi
+        local bashrc_dir="$HOME/.bashrc"
+        if ! (command -v bash >/dev/null); then
+          if [ -e "$bashrc_dir" ]; then
+            echo 'eval "$(zoxide init --cmd cd bash)"' >>$bashrc_dir
+          fi
+        fi
+      else
+        echo "zoxide is already installed"
+      fi
+    fi
+  fi
 }
 
 packages_install
