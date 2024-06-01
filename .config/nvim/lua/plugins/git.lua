8e50f80950dee4cd8a6fb26a5123fd83ac3834dd
yamayama <rihanito30@gmail.com>
2024-06-02 02:03:12 +0900

    update: eventからBufReadPreを削除

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,7 +1,7 @@
 return {
     {
         "lewis6991/gitsigns.nvim",
-        event = { "BufReadPre", "BufReadPost", "BufNewFile" },
+        event = { "BufReadPost", "BufNewFile" },
         config = function()
             require("gitsigns").setup({})
         end,
