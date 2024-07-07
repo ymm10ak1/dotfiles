36e3a7bab6d112d1dc3772c712d91913d08b1018
ymm10 <rihanito30@gmail.com>
2024-07-07 10:00:55 +0900

    update: yesコマンドからecho yに変更

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -58,7 +58,7 @@ packages_install() {
       # starshipのインストール
       if ! (has "starship"); then
         echo "Install starship"
-        yes | curl -sS https://starship.rs/install.sh | sh
+        echo "y" | curl -sS https://starship.rs/install.sh | sh
       else
         echo "starship is already installed"
       fi
