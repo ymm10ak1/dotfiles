7af9682759a55fe80742a4a641e4efc0c1f2fe15
ymm10 <rihanito30@gmail.com>
2024-07-14 04:06:10 +0900

    update: lazygit.nvimの削除

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,7 +1,7 @@
 return {
     {
         "lewis6991/gitsigns.nvim",
-        event = { "BufReadPost" },
+        event = { "BufRead" },
         config = function()
             require("gitsigns").setup({})
         end,
@@ -13,25 +13,12 @@ return {
         config = true,
     },
     {
-        "kdheepak/lazygit.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = { "nvim-lua/plenary.nvim" },
+        "FabijanZulj/blame.nvim",
+        event = { "BufRead" },
+        cmd = { "BlameToggle" },
         config = function()
-            vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { noremap = true, silent = true })
-            -- LazyGit起動時にESCを無効化
-            vim.api.nvim_create_augroup("LazygitKeyMapping", {})
-            vim.api.nvim_create_autocmd("TermOpen", {
-                group = "LazygitKeyMapping",
-                pattern = "*",
-                callback = function()
-                    local filetype = vim.bo.filetype
-                    if filetype == "lazygit" then
-                        -- なぜ動くのかはわからない
-                        vim.api.nvim_buf_set_keymap(0, "t", "<ESC>", "<ESC>", { silent = true })
-                        vim.api.nvim_buf_set_keymap(0, "t", "<C-v><ESC>", [[<C-\><C-n>]], { silent = true })
-                    end
-                end,
-            })
+            require("blame").setup({})
+            vim.keymap.set("n", "<leader>gb", "<cmd>BlameToggle<cr>")
         end,
     },
 }
