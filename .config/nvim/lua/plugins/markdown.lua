773d886e93e9a1f8101c652d701c08e856d3b7b0
ymm10 <rihanito30@gmail.com>
2024-07-21 21:12:55 +0900

    update: render-markdownをkeysで読み込むように変更

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,23 +1,25 @@
 return {
     {
         "MeanderingProgrammer/markdown.nvim",
-        ft = { "markdown" },
+        ft = "markdown",
         name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
         dependencies = {
             "nvim-treesitter/nvim-treesitter", -- Mandatory
             "nvim-tree/nvim-web-devicons", -- Optional but recommended
+        },
+        keys = {
+            { "<M-r>", "<cmd>RenderMarkdown toggle<cr>" },
         },
         config = function()
             require("render-markdown").setup({
                 -- Markdownをデフォルトではレンダリングしない
                 enabled = false,
             })
-            vim.keymap.set("n", "<M-r>", "<cmd>RenderMarkdownToggle<CR>")
         end,
     },
     {
         "ixru/nvim-markdown",
-        ft = { "markdown" },
+        ft = "markdown",
         event = { "BufReadPre", "BufNewFile" },
     },
 }
