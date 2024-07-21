6c95087ca549dbf73c5e09343861c6ad164f0d6b
ymm10 <rihanito30@gmail.com>
2024-07-21 21:10:24 +0900

    update: lspに接続したときにkeymapを設定するよう変更

--- .config/nvim/lua/plugins/lspsaga.lua
+++ .config/nvim/lua/plugins/lspsaga.lua
@@ -24,19 +24,24 @@ return {
                 extend_gitsigns = false,
             }
         })
-        -- diagnostic
-        keymap("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<cr>")
-        keymap("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<cr>")
-        keymap("n", "ge", "<cmd>Lspsaga show_workspace_diagnostics ++float<cr>")
-        -- code action
-        keymap("n", "ga", "<cmd>Lspsaga code_action<cr>")
-        -- hover
-        keymap("n", "K", "<cmd>Lspsaga hover_doc<cr>")
-        -- finder
-        keymap("n", "gn", "<cmd>Lspsaga finder<cr>")
-        -- definition
-        keymap("n", "gd", "<cmd>Lspsaga peek_definition<cr>")
-        keymap("n", "gy", "<cmd>Lspsaga peek_type_definition<cr>")
+        vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function (ev)
+                local opts = { buffer = ev.buf }
+                -- diagnostic
+                keymap("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<cr>", opts)
+                keymap("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<cr>", opts)
+                keymap("n", "ge", "<cmd>Lspsaga show_workspace_diagnostics ++float<cr>", opts)
+                -- code action
+                keymap("n", "ga", "<cmd>Lspsaga code_action<cr>", opts)
+                -- hover
+                keymap("n", "K", "<cmd>Lspsaga hover_doc<cr>", opts)
+                -- finder
+                keymap("n", "gn", "<cmd>Lspsaga finder<cr>", opts)
+                -- definition
+                keymap("n", "gd", "<cmd>Lspsaga peek_definition<cr>", opts)
+                keymap("n", "gy", "<cmd>Lspsaga peek_type_definition<cr>", opts)
+            end
+        })
     end,
     depedencies = {
         "nvim-treesitter/nvim-treesitter",
