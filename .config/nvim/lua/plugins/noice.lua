e6a7a2110a2826296983e93c3e107f22108cd772
ymm10 <rihanito30@gmail.com>
2024-07-26 18:32:33 +0900

    update: setupからoptsに設定を移動、keysでhistoryを読み込むように変更

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -6,37 +6,41 @@ return {
         "MunifTanjim/nui.nvim",
         "rcarriga/nvim-notify",
     },
-    config = function()
-        require("noice").setup({
-            messages = {
-                view = "mini",
-                view_error = "notify",
-                view_warn = "notify",
-                view_history = "mini",
-                view_search = false,
+    keys = {
+        {
+            "<leader>nh",
+            function()
+                require("noice").cmd("history")
+            end,
+            desc = "noice history",
+        },
+    },
+    opts = {
+        messages = {
+            view = "mini",
+            view_error = "notify",
+            view_warn = "notify",
+            view_history = "mini",
+            view_search = false,
+        },
+        lsp = {
+            progress = {
+                enabled = false,
             },
-            lsp = {
-                progress = {
-                    enabled = false,
-                },
-                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                    ["cmp.entry.get_documentation"] = true,
-                },
+            -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true,
             },
-            -- you can enable a preset for easier configuration
-            presets = {
-                bottom_search = true, -- use a classic bottom cmdline for search
-                command_palette = true, -- position the cmdline and popupmenu together
-                long_message_to_split = true, -- long messages will be sent to a split
-                inc_rename = false, -- enables an input dialog for inc-rename.nvim
-                lsp_doc_border = false, -- add a border to hover docs and signature help
-            },
-        })
-        vim.keymap.set("n", "<leader>nh", function()
-            require("noice").cmd("history")
-        end)
-    end,
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+            bottom_search = true, -- use a classic bottom cmdline for search
+            command_palette = true, -- position the cmdline and popupmenu together
+            long_message_to_split = true, -- long messages will be sent to a split
+            inc_rename = false, -- enables an input dialog for inc-rename.nvim
+            lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+    },
 }
