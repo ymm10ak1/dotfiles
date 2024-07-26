e1fda0957026369221c01109fd52ae109f7929d2
ymm10 <rihanito30@gmail.com>
2024-07-26 18:26:58 +0900

    remove: 使わないプラグインの削除

--- .config/nvim/lua/plugins/surround.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    enabled = false,
-    event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup({})
-    end,
-}
