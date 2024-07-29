f2f512cd9c1e7c5a297a4213f579aced743c1f4d
ymm10 <rihanito30@gmail.com>
2024-07-29 17:54:11 +0900

    update: eventをVeryLazyに変更、setup内で渡していた設定をoptsに移動

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,12 +1,10 @@
 return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    event = { "BufNewFile", "BufReadPre" },
-    config = function()
-        require("lualine").setup({
-            options = {
-                globalstatus = true,
-            },
-        })
-    end,
+    event = "VeryLazy",
+    opts = {
+        options = {
+            globalstatus = true,
+        },
+    },
 }
