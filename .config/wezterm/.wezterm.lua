5409be9c8827dc937d1a627cc9d065a59803fc11
ymm10 <rihanito30@gmail.com>
2024-08-03 03:34:38 +0900

    update: WSLのディストリビューションを追加、leader, keysをローカルで定義するように変更

--- .config/wezterm/.wezterm.lua
+++ .config/wezterm/.wezterm.lua
@@ -2,7 +2,8 @@ local wezterm = require("wezterm")
 
 local wsl_domains = wezterm.default_wsl_domains()
 for _, dom in ipairs(wsl_domains) do
-    if dom.name == "WSL:Ubuntu-22.04" then
+    -- 今WSLで使用しているディストリビューションに対応
+    if dom.name == "WSL:Ubuntu-22.04" or dom.name == "WSL:Ubuntu-20.04" or dom.name == "WSL:Ubuntu" then
         dom.default_cwd = "~"
     end
 end
@@ -20,6 +21,72 @@ wezterm.on("new-tab-button-click", function(window, pane)
     )
     return false
 end)
+
+wezterm.on("format-tab-title", function(tab)
+    return tab.active_pane.title
+end)
+
+local leader = { key = "f", mods = "CTRL", timeout_milliseconds = 1000 }
+local keys = {
+    -- 新しいWindow
+    {
+        key = "n",
+        mods = "CTRL|SHIFT",
+        action = act.SpawnCommandInNewWindow({
+            cwd = "~",
+            domain = "CurrentPaneDomain",
+        }),
+    },
+    -- 新しいタブ
+    {
+        key = "c",
+        mods = "LEADER",
+        action = act.SpawnCommandInNewTab({
+            cwd = "~",
+            domain = "CurrentPaneDomain",
+        }),
+    },
+    -- 水平分割
+    {
+        key = "s",
+        mods = "LEADER",
+        action = act.SplitHorizontal({
+            cwd = "~",
+            domain = "CurrentPaneDomain",
+        }),
+    },
+    -- 垂直分割
+    {
+        key = "v",
+        mods = "LEADER",
+        action = act.SplitVertical({
+            cwd = "~",
+            domain = "CurrentPaneDomain",
+        }),
+    },
+    -- pane移動
+    { key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+    { key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+    { key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+    { key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+    -- paneサイズ変更
+    { key = "H", mods = "LEADER", action = act.AdjustPaneSize({ "Left", 5 }) },
+    { key = "J", mods = "LEADER", action = act.AdjustPaneSize({ "Down", 5 }) },
+    { key = "K", mods = "LEADER", action = act.AdjustPaneSize({ "Up", 5 }) },
+    { key = "L", mods = "LEADER", action = act.AdjustPaneSize({ "Right", 5 }) },
+    -- タブ削除
+    { key = "q", mods = "LEADER", action = act.CloseCurrentTab({ confirm = true }) },
+    -- ペイン削除
+    { key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
+    -- タブ移動
+    { key = "[", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+    { key = "]", mods = "LEADER", action = act.ActivateTabRelative(1) },
+    -- Copy/Paste
+    { key = "C", mods = "CTRL", action = act.CopyTo("Clipboard") },
+    { key = "V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+    -- Fullscrenn
+    { key = "f", mods = "SHIFT|ALT", action = act.ToggleFullScreen },
+}
 
 return {
     wsl_domains = wsl_domains,
@@ -39,65 +106,6 @@ return {
     default_domain = "WSL:Ubuntu-22.04",
     default_prog = { "wsl.exe" },
     hide_tab_bar_if_only_one_tab = true,
-    leader = { key = "f", mods = "CTRL", timeout_milliseconds = 2000 },
-    keys = {
-        -- 新しいWindow
-        {
-            key = "n",
-            mods = "CTRL|SHIFT",
-            action = act.SpawnCommandInNewWindow({
-                cwd = "~",
-                domain = "CurrentPaneDomain",
-            }),
-        },
-        -- 新しいタブ
-        {
-            key = "c",
-            mods = "LEADER",
-            action = act.SpawnCommandInNewTab({
-                cwd = "~",
-                domain = "CurrentPaneDomain",
-            }),
-        },
-        -- 水平分割
-        {
-            key = "s",
-            mods = "LEADER",
-            action = act.SplitHorizontal({
-                cwd = "~",
-                domain = "CurrentPaneDomain",
-            }),
-        },
-        -- 垂直分割
-        {
-            key = "v",
-            mods = "LEADER",
-            action = act.SplitVertical({
-                cwd = "~",
-                domain = "CurrentPaneDomain",
-            }),
-        },
-        -- pane移動
-        { key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
-        { key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
-        { key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
-        { key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
-        -- paneサイズ変更
-        { key = "H", mods = "LEADER", action = act.AdjustPaneSize({ "Left", 5 }) },
-        { key = "J", mods = "LEADER", action = act.AdjustPaneSize({ "Down", 5 }) },
-        { key = "K", mods = "LEADER", action = act.AdjustPaneSize({ "Up", 5 }) },
-        { key = "L", mods = "LEADER", action = act.AdjustPaneSize({ "Right", 5 }) },
-        -- タブ削除
-        { key = "q", mods = "LEADER", action = act.CloseCurrentTab({ confirm = true }) },
-        -- ペイン削除
-        { key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
-        -- タブ移動
-        { key = "[", mods = "LEADER", action = act.ActivateTabRelative(-1) },
-        { key = "]", mods = "LEADER", action = act.ActivateTabRelative(1) },
-        -- Copy/Paste
-        { key = "C", mods = "CTRL", action = act.CopyTo("Clipboard") },
-        { key = "V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
-        -- Fullscrenn
-        { key = "f", mods = "SHIFT|ALT", action = act.ToggleFullScreen },
-    },
+    leader = leader,
+    keys = keys,
 }
