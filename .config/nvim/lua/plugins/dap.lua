72e9343bc494c24a91b0514b20dfae07d8b6dc36
ymm10 <rihanito30@gmail.com>
2024-08-21 11:00:23 +0900

    update: 起動イベントをLspAttachに変更、keysで読み込むように変更

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -4,40 +4,52 @@ return {
     {
         "mfussenegger/nvim-dap",
         cond = vscode,
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            vim.keymap.set("n", "<F5>", function()
-                require("dap").continue()
-            end)
-            vim.keymap.set("n", "<F10>", function()
-                require("dap").step_over()
-            end)
-            vim.keymap.set("n", "<F11>", function()
-                require("dap").step_into()
-            end)
-            vim.keymap.set("n", "<F12>", function()
-                require("dap").step_out()
-            end)
-            vim.keymap.set("n", "<leader>B", function()
-                require("dap").toggle_breakpoint()
-            end)
-            vim.keymap.set("n", "<leader>pm", function()
-                require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
-            end)
-            vim.keymap.set("n", "<leader>dr", function()
-                require("dap").repl.open()
-            end)
-            vim.keymap.set("n", "<leader>dl", function()
-                require("dap").run_last()
-            end)
-        end,
+        event = "LspAttach",
+        keys = {
+            {
+                "<F5>",
+                function()
+                    require("dap").continue()
+                end,
+                desc = "Continue",
+            },
+            {
+                "<F10>",
+                function()
+                    require("dap").step_over()
+                end,
+                desc = "Step Over",
+            },
+            {
+                "<F11>",
+                function()
+                    require("dap").step_into()
+                end,
+                desc = "Step Into",
+            },
+            {
+                "<F12>",
+                function()
+                    require("dap").step_out()
+                end,
+                desc = "Step Out",
+            },
+            {
+                "<leader>db",
+                function()
+                    require("dap").toggle_breakpoint()
+                end,
+                desc = "Toggle Breakpoint",
+            },
+        },
     },
     {
         "rcarriga/nvim-dap-ui",
         cond = vscode,
-        event = { "BufReadPre", "BufNewFile" },
+        event = "LspAttach",
         dependencies = {
             "mfussenegger/nvim-dap",
+            "nvim-neotest/nvim-nio",
         },
     },
 }
