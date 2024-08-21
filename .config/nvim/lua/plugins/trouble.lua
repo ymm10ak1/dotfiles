ecc4f643ae82f9c6f7b10f8143404493083f193c
ymm10 <rihanito30@gmail.com>
2024-08-21 11:23:38 +0900

    update: LSP Troubleのkeymapを変更

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -13,7 +13,7 @@ return {
         { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
         { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
         {
-            "<leader>cl",
+            "<leader>xl",
             "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
             desc = "LSP Definitions / references / ... (Trouble)",
         },
