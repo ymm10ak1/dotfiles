180891df50aa20b355c5bb02ea21a25405086727
ymm10 <rihanito30@gmail.com>
2024-08-21 10:57:58 +0900

    update: keysで遅延読み込みするように変更

--- .config/nvim/lua/plugins/close-buffers.lua
+++ .config/nvim/lua/plugins/close-buffers.lua
@@ -3,20 +3,19 @@ local vscode = require("utils").vscode_check
 return {
     "kazhala/close-buffers.nvim",
     cond = vscode,
-    event = { "BufReadPre", "BufNewFile"},
-    config = function ()
-        require("close_buffers").setup({
-            preserve_window_layout = {"this"},
-            next_buffer_cmt = function (windows)
-                require("bufferline").cycle(1)
-                local bufnr = vim.api.nvim_get_current_buf()
+    -- event = { "BufReadPre", "BufNewFile" },
+    keys = {
+        { "<leader>to", "<cmd>BDelete other<cr>", desc = "BDelete other" },
+    },
+    opts = {
+        preserve_window_layout = { "this" },
+        next_buffer_cmt = function(windows)
+            require("bufferline").cycle(1)
+            local bufnr = vim.api.nvim_get_current_buf()
 
-                for _, window in ipairs(windows) do
-                    vim.api.nvim_win_set_buf(window, bufnr)
-                end
-            end,
-        })
-        vim.keymap.set("n", "<leader>to", "<cmd>BDelete other<cr>")
-        vim.keymap.set("n", "<leader>tc", "<cmd>BDelete this<cr>")
-    end
+            for _, window in ipairs(windows) do
+                vim.api.nvim_win_set_buf(window, bufnr)
+            end
+        end,
+    },
 }
