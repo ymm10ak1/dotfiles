14603bcd939b37ac34b95b5c13c87fae6befd3d9
ymm10 <rihanito30@gmail.com>
2024-08-21 11:18:04 +0900

    update: cmdでのみ読み込むように変更

--- .config/nvim/lua/plugins/quickrun.lua
+++ .config/nvim/lua/plugins/quickrun.lua
@@ -3,8 +3,5 @@ local vscode = require("utils").vscode_check
 return {
     "thinca/vim-quickrun",
     cond = vscode,
-    event = "VeryLazy",
-    -- config = function ()
-    --     vim.keymap.set("n", "<leader>r", "<cmd>Quickrun")
-    -- <end
+    cmd = "Quickrun",
 }
