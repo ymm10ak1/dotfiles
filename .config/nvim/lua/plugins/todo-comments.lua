5c900cb1ea9d5a5180a6a0f3a92fc818bd5a5026
ymm10 <rihanito30@gmail.com>
2024-08-21 11:21:16 +0900

    update: TodoCommentsをQuickFixで表示keymapを追加

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -3,11 +3,10 @@ return {
     "folke/todo-comments.nvim",
     cond = vscode,
     -- event = { "BufRead", "BufNewFile" },
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
+    dependencies = { "nvim-lua/plenary.nvim" },
     opts = {},
     keys = {
         { "<leader>tt", "<cmd>TodoTelescope<CR>", desc = "TodoComments Telescope" },
+        { "<leader>tq", "<cmd>TodoQuickFix<CR>", desc = "TodoComments QuickFix" },
     },
 }
