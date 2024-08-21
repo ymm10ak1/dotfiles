e3ec5553efad8ca5093633dfb42d492932603260
ymm10 <rihanito30@gmail.com>
2024-08-21 11:24:20 +0900

    update: startuptimeを30回試すように変更

--- .config/nvim/lua/plugins/vim-startuptime.lua
+++ .config/nvim/lua/plugins/vim-startuptime.lua
@@ -7,6 +7,6 @@ return {
     cmd = "StartupTime",
     -- init is called during startup. Configuration for vim plugins typically should be set in an init function
     init = function()
-        vim.g.startuptime_tries = 10
+        vim.g.startuptime_tries = 30
     end,
 }
