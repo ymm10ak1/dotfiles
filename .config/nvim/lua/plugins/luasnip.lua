8cf60731595a0b7635fe9c396acbdcbcbbe47730
ymm10ak1 <rihanito30@gmail.com>
2024-08-29 12:46:10 +0900

    update: luasnipの設定をnvim-cmp.luaで設定するように変更

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -3,12 +3,8 @@ local vscode = require("utils").vscode_check
 return {
     "L3MON4D3/LuaSnip",
     cond = vscode,
-    event = "InsertEnter",
+    -- event = "InsertEnter",
     dependencies = { "rafamadriz/friendly-snippets" },
     version = "v2.*",
     build = "make install_jsregexp",
-    config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
-        require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/snippets" } })
-    end,
 }
