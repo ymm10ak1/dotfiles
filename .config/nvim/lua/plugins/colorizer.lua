bf003f5759c704f588caee9adad21766e8a93ca0
ymm10ak1 <rihanito30@gmail.com>
2024-09-09 23:17:07 +0900

    remove: ファイル名をnvim-highlight-colors.luaに変更

--- .config/nvim/lua/plugins/colorizer.lua
+++
@@ -1,8 +0,0 @@
-local vscode = require("utils").vscode_check
-
-return {
-    "brenoprata10/nvim-highlight-colors",
-    cond = vscode,
-    event = { "BufRead", "BufNewFile" },
-    config = true,
-}
