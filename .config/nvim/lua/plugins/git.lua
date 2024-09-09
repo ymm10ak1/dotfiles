00c7bbc5ac8f77e207b442b2b29da0cbe20d2660
ymm10ak1 <rihanito30@gmail.com>
2024-09-09 23:21:14 +0900

    update: 依存しているプラグインをdependenciesから分けるように変更、defaultkeymapのsを無効にするように変更

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -10,12 +10,18 @@ return {
     {
         "sindrets/diffview.nvim",
         cond = vscode,
-        dependencies = { "nvim-tree/nvim-web-devicons" },
+        -- dependencies = { "nvim-tree/nvim-web-devicons" },
         cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+        opts = {
+            keymaps = {
+                file_panel = {
+                    ["s"] = false,
+                },
+            },
+        },
         init = function()
             vim.keymap.set("n", "<leader>dq", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" })
         end,
-        config = true,
     },
     {
         "FabijanZulj/blame.nvim",
