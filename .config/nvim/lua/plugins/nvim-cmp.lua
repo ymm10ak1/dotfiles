5e4bdc5cb4b082fe8b92c14c50d08539f9b56138
ymm10ak1 <rihanito30@gmail.com>
2024-09-09 23:45:31 +0900

    update: 依存プラグインの遅延読み込み、lspkindの設定を変更、Enterキーの設定を変更
    依存プラグインを遅延読込するためにdependenciesで読み込まずそれぞれ分けて読み込むように変更
    lspkindでnvim-highlight-colorsを使用するように変更
    補完候補を選択していないときはEnterキー押しても改行するように変更

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,136 +1,156 @@
 local vscode = require("utils").vscode_check
 
 return {
-    "hrsh7th/nvim-cmp",
-    cond = vscode,
-    event = { "InsertEnter", "CmdlineEnter" },
-    dependencies = {
-        { "L3MON4D3/LuaSnip" },
-        { "saadparwaiz1/cmp_luasnip", event = "InsertEnter" },
-        { "hrsh7th/cmp-nvim-lsp", event = "InsertEnter" },
-        { "hrsh7th/cmp-nvim-lsp-signature-help", event = "InsertEnter" },
-        { "hrsh7th/cmp-buffer", event = "InsertEnter" },
-        { "hrsh7th/cmp-path", event = "InsertEnter" },
-        { "hrsh7th/cmp-cmdline", event = { "ModeChanged", "CmdlineChanged" } },
-        { "onsails/lspkind.nvim", event = "InsertEnter" },
-    },
-    config = function()
-        local cmp = require("cmp")
-        local lspkind = require("lspkind")
+    { "saadparwaiz1/cmp_luasnip", cond = vscode, event = "InsertEnter" },
+    { "hrsh7th/cmp-nvim-lsp", cond = vscode, event = "InsertEnter" },
+    { "hrsh7th/cmp-nvim-lsp-signature-help", cond = vscode, event = "InsertEnter" },
+    { "hrsh7th/cmp-buffer", cond = vscode, event = "InsertEnter" },
+    { "hrsh7th/cmp-path", cond = vscode, event = "InsertEnter" },
+    { "hrsh7th/cmp-cmdline", cond = vscode, event = { "CmdlineChanged", "ModeChanged" } },
+    { "onsails/lspkind.nvim", cond = vscode, event = "InsertEnter" },
+    {
+        "hrsh7th/nvim-cmp",
+        cond = vscode,
+        event = { "InsertEnter", "CmdlineEnter" },
+        -- dependencies = {
+        --     { "L3MON4D3/LuaSnip" },
+        --     { "saadparwaiz1/cmp_luasnip", event = "InsertEnter" },
+        --     { "hrsh7th/cmp-nvim-lsp", event = "InsertEnter" },
+        --     { "hrsh7th/cmp-nvim-lsp-signature-help", event = "InsertEnter" },
+        --     { "hrsh7th/cmp-buffer", event = "InsertEnter" },
+        --     { "hrsh7th/cmp-path", event = "InsertEnter" },
+        --     { "hrsh7th/cmp-cmdline", event = { "ModeChanged", "CmdlineChanged" } },
+        --     { "onsails/lspkind.nvim", event = "InsertEnter" },
+        -- },
+        config = function()
+            local cmp = require("cmp")
+            local lspkind = require("lspkind")
 
-        require("luasnip.loaders.from_vscode").lazy_load()
-        require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/snippets" } })
+            require("luasnip.loaders.from_vscode").lazy_load()
+            require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/snippets" } })
 
-        cmp.setup({
-            mapping = {
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    else
-                        fallback()
-                    end
-                end, { "i", "c", "s" }),
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    else
-                        fallback()
-                    end
-                end, { "i", "c", "s" }),
-                ["<C-j>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    else
-                        fallback()
-                    end
-                end, { "i", "c", "s" }),
-                ["<C-k>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    else
-                        fallback()
-                    end
-                end, { "i", "c", "s" }),
-                ["<C-u>"] = cmp.mapping(function(fallback)
-                    if cmp.visible_docs() then
-                        cmp.scroll_docs(-4)
-                    else
-                        fallback()
-                    end
-                end, { "i" }),
-                ["<C-d>"] = cmp.mapping(function(fallback)
-                    if cmp.visible_docs() then
-                        cmp.scroll_docs(4)
-                    else
-                        fallback()
-                    end
-                end, { "i" }),
-                ["<CR>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.confirm({ select = true })
-                    else
-                        fallback()
-                    end
-                end),
-            },
-            window = {
-                -- completion = cmp.config.window.bordered(),
-                documentation = cmp.config.window.bordered(),
-            },
-            snippet = {
-                expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
-                end,
-            },
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "nvim_lsp_signature_help" },
-                { name = "luasnip" },
-                { name = "path" },
-            }, {
-                {
-                    name = "buffer",
-                    option = {
-                        keyword_length = 2,
-                    },
+            cmp.setup({
+                mapping = {
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "c", "s" }),
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "c", "s" }),
+                    ["<C-j>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "c", "s" }),
+                    ["<C-k>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "c", "s" }),
+                    ["<C-u>"] = cmp.mapping(function(fallback)
+                        if cmp.visible_docs() then
+                            cmp.scroll_docs(-4)
+                        else
+                            fallback()
+                        end
+                    end, { "i" }),
+                    ["<C-d>"] = cmp.mapping(function(fallback)
+                        if cmp.visible_docs() then
+                            cmp.scroll_docs(4)
+                        else
+                            fallback()
+                        end
+                    end, { "i" }),
+                    ["<CR>"] = cmp.mapping({
+                        i = function(fallback)
+                            if cmp.visible() and cmp.get_active_entry() then
+                                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+                            else
+                                fallback()
+                            end
+                        end,
+                        s = cmp.mapping.confirm({ select = true }),
+                    }),
                 },
-            }),
-            formatting = {
-                format = lspkind.cmp_format({
-                    mode = "symbol",
-                    maxwidth = 50,
-                    ellipsis_char = "...",
-                    show_labelDatails = true,
+                window = {
+                    -- completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "nvim_lsp_signature_help" },
+                    { name = "luasnip" },
+                    { name = "path" },
+                }, {
+                    {
+                        name = "buffer",
+                        option = {
+                            keyword_length = 2,
+                        },
+                    },
                 }),
-            },
-        })
+                formatting = {
+                    format = function(entry, item)
+                        local color_item = require("nvim-highlight-colors").format(entry, { kind = item.kind })
+                        item = lspkind.cmp_format({
+                            mode = "symbol",
+                            maxwidth = 50,
+                            ellipsis_char = "...",
+                            show_labelDatails = true,
+                        })(entry, item)
+                        if color_item.abbr_hl_group then
+                            item.kind_hl_group = color_item.abbr_hl_group
+                            item.kind = color_item.abbr
+                        end
+                        return item
+                    end,
+                },
+            })
 
-        -- `/, ?` cmdline setup
-        cmp.setup.cmdline({ "/", "?" }, {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = {
-                {
-                    name = "buffer",
-                    option = {
-                        keyword_length = 1,
+            -- `/, ?` cmdline setup
+            cmp.setup.cmdline({ "/", "?" }, {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    {
+                        name = "buffer",
+                        option = {
+                            keyword_length = 1,
+                        },
                     },
                 },
-            },
-        })
+            })
 
-        -- `:` cmdline setup
-        cmp.setup.cmdline(":", {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({
-                { name = "path" },
-            }, {
-                {
-                    name = "cmdline",
-                    option = {
-                        ignore_cmds = { "Man", "!" },
+            -- `:` cmdline setup
+            cmp.setup.cmdline(":", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = "path" },
+                }, {
+                    {
+                        name = "cmdline",
+                        option = {
+                            ignore_cmds = { "Man", "!" },
+                        },
                     },
-                },
-            }),
-        })
-    end,
+                }),
+            })
+        end,
+    },
 }
