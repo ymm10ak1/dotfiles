119d158f88af5812ec581a07149e91ebabf8306e
ymm10ak1 <rihanito30@gmail.com>
2024-09-22 17:20:48 +0900

    update: archlinux用のパッケージをインストールするスクリプトを追加

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -eu
 
 INSTALL_LIST=(git curl build-essential xsel unzip zsh sqlite3 libsqlite3-dev zip wget)
+ARCH_INSTALL_LIST=(git curl sqlite3 xsel zip unzip wget rustup)
 
 has() {
   type -p "$1" >/dev/null 2>&1
@@ -28,17 +29,6 @@ packages_install() {
         fi
       done
 
-      # aquaのインストール
-      if ! (has "aqua"); then
-        echo "Install aqua"
-        curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.0.1/aqua-installer
-        echo "fb4b3b7d026e5aba1fc478c268e8fbd653e01404c8a8c6284fdba88ae62eda6a  aqua-installer" | sha256sum -c
-        chmod +x aqua-installer
-        ./aqua-installer
-      else
-        echo "aqua is already installed"
-      fi
-
       # rustのインストール
       if ! (has "rustup"); then
         echo "Install rust"
@@ -46,22 +36,38 @@ packages_install() {
       else
         echo "rust is already installed"
       fi
-
-      # miseのインストール
-      if ! (has "mise"); then
-        echo "Install mise"
-        curl https://mise.run | sh
-        "$HOME/.local/bin/mise" --version
-      else
-        echo "mise is already installed"
-      fi
-
-      # シェルをbashからzshに変更
-      if [[ "$SHELL" =~ "bash" ]]; then
-        echo "Change zsh"
-        sudo chsh "$(id -un)" -s "$(which zsh)"
-      fi
     fi
+  elif [[ -e /etc/arch-release ]]; then
+    sudo pacman -Syyu --disable-sandbox
+    for pkg in "${ARCH_INSTALL_LIST[@]}"; do
+      sudo pacman -S --noconfirm --needed "$pkg"
+    done
+  fi
+
+  # aquaのインストール
+  if ! (has "aqua"); then
+    echo "Install aqua"
+    curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.0.1/aqua-installer
+    echo "fb4b3b7d026e5aba1fc478c268e8fbd653e01404c8a8c6284fdba88ae62eda6a  aqua-installer" | sha256sum -c
+    chmod +x aqua-installer
+    ./aqua-installer
+  else
+    echo "aqua is already installed"
+  fi
+
+  # miseのインストール
+  if ! (has "mise"); then
+    echo "Install mise"
+    curl https://mise.run | sh
+    "$HOME/.local/bin/mise" --version
+  else
+    echo "mise is already installed"
+  fi
+
+  # シェルをbashからzshに変更
+  if [[ "$SHELL" =~ "bash" ]]; then
+    echo "Change zsh"
+    sudo chsh "$(id -un)" -s "$(which zsh)"
   fi
 }
 
