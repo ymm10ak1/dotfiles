85f4573c231bce64b68ab5d547e6eb022f41421d
ymm10ak1 <rihanito30@gmail.com>
2024-09-25 20:17:53 +0900

    update: sは他のプラグインでも使用していることがあるため、Flashのkeymapをmに変更

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -4,10 +4,9 @@ return {
     opts = {},
     keys = {
         -- stylua: ignore start
-        { "s", mode = { "n", "x", "o" }, function () require("flash").jump() end, desc = "Flash" },
+        { "m", mode = { "n", "x", "o" }, function () require("flash").jump() end, desc = "Flash" },
         { "r", mode = { "o" }, function () require("flash").remote() end, desc = "Remote Flash" },
         { "R", mode = { "x", "o" }, function () require("flash").treesitter_search() end, desc = "Treesitter Search" },
-        { "<C-s>", mode = { "c" }, function () require("flash").toggle() end, desc = "Toggle Flash Search" },
         -- stylua: ignore end
         {
             "<CR>",
