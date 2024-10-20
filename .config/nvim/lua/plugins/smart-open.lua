4a1bb9e7aae5087a3a6865313413944d1ec25549
ymm10ak1 <66200742+ymm10ak1@users.noreply.github.com>
2024-10-20 16:41:16 +0900

    update: いらないコメント等を削除

--- .config/nvim/lua/plugins/smart-open.lua
+++ .config/nvim/lua/plugins/smart-open.lua
@@ -1,15 +1,11 @@
 local vscode = require("utils").vscode_check
 
 return {
-    -- { "kkharji/sqlite.lua", cond = vscode },
-    -- { "nvim-telescope/telescope-fzy-native.nvim", cond = vscode },
-    {
-        "danielfalk/smart-open.nvim",
-        cond = vscode,
-        branch = "0.2.x",
-        dependencies = {
-            { "kkharji/sqlite.lua" },
-            { "nvim-telescope/telescope-fzy-native.nvim" },
-        },
+    "danielfalk/smart-open.nvim",
+    cond = vscode,
+    branch = "0.2.x",
+    dependencies = {
+        { "kkharji/sqlite.lua" },
+        { "nvim-telescope/telescope-fzy-native.nvim" },
     },
 }
