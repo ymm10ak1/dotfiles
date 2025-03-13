7e1f736979a77c0b13992cb4fdfd58ca53b20ab7
ymm10ak1 <66200742+ymm10ak1@users.noreply.github.com>
2025-03-13 16:59:30 +0900

    update: 遅延起動するキーマップを変更

--- .config/nvim/lua/plugins/hop.lua
+++ .config/nvim/lua/plugins/hop.lua
@@ -2,7 +2,7 @@ return {
   "smoka7/hop.nvim",
   version = "*",
   keys = {
-    { "<leader>fj", "<Cmd>HopLineStart<CR>", desc = "HopLineStart" },
+    { "<leader>jk", "<Cmd>HopLineStart<CR>", desc = "HopLineStart" },
   },
   opts = {
     keys = "etovxqpdygfblzhckisuran",
