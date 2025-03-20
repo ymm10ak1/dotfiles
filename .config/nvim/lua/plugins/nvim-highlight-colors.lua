b98de46c046abb33c1205d7ba3e661d934f4d6bb
ymm10ak1 <66200742+ymm10ak1@users.noreply.github.com>
2025-03-21 03:36:17 +0900

    update: 読み込みeventを変更

--- .config/nvim/lua/plugins/nvim-highlight-colors.lua
+++ .config/nvim/lua/plugins/nvim-highlight-colors.lua
@@ -3,6 +3,6 @@ local vscode = require("utils").vscode_check
 return {
   "brenoprata10/nvim-highlight-colors",
   cond = vscode,
-  -- event = { "BufRead", "BufNewFile" },
+  event = { "BufRead", "BufNewFile" },
   opts = {},
 }
