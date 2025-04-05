da4e9310faf7dd92a93c3aefc80333c21fa582d9
ymm10ak1 <66200742+ymm10ak1@users.noreply.github.com>
2025-04-05 17:23:00 +0900

    update: dashboardにdotfilesを追加、使わない機能を無効

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -9,7 +9,39 @@ return {
   ---@type snacks.Config
   opts = {
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = {
+      enabled = true,
+      preset = {
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          {
+            icon = " ",
+            key = "d",
+            desc = "Dotfiles",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = '~/dotfiles', hidden = true})",
+          },
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+          },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+      sections = {
+        { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
+    },
     explorer = { enabled = true },
     input = { enabled = true },
     picker = {
@@ -48,6 +80,7 @@ return {
     terminal = { enabled = false },
     animate = { enabled = false },
     dim = { enabled = false },
+    gitbrowse = { enabled = false },
   },
   keys = {
     -- stylua: ignore start
