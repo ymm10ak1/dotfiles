3df2a1864e15747a55d2908ed7b08ecc98062fb5
ymm10ak1 <66200742+ymm10ak1@users.noreply.github.com>
2025-05-04 13:33:00 +0900

    update: CamelCaseMotion_geを追加

--- .config/nvim/lua/plugins/camelcasemotion.lua
+++ .config/nvim/lua/plugins/camelcasemotion.lua
@@ -4,5 +4,6 @@ return {
     { "<leader>w", "<Plug>CamelCaseMotion_w", mode = { "n", "x" } },
     { "<leader>b", "<Plug>CamelCaseMotion_b", mode = { "n", "x" } },
     { "<leader>e", "<Plug>CamelCaseMotion_e", mode = { "n", "x" } },
+    { "<leader>ge", "<Plug>CamelCaseMotion_ge", mode = { "n", "x" } },
   },
 }
