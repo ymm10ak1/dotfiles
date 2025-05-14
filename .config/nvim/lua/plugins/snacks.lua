aab0da37f3df01f20474d6d2f46fd2d7ee05cab8
ymm10ak1 <66200742+ymm10ak1@users.noreply.github.com>
2025-05-14 22:31:31 +0900

    update: picker時のキーマップ変更、dashboardの設定を分割

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -2,43 +2,10 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
-  -- dependencies = { "echasnovski/mini.icons" },
   ---@type snacks.Config
   opts = {
     bigfile = { enabled = true },
-    dashboard = {
-      enabled = true,
-      preset = {
-        ---@type snacks.dashboard.Item[]
-        keys = {
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          {
-            icon = " ",
-            key = "d",
-            desc = "Dotfiles",
-            action = ":lua Snacks.dashboard.pick('files', {cwd = '~/dotfiles', hidden = true})",
-          },
-          {
-            icon = " ",
-            key = "c",
-            desc = "Config",
-            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-          },
-          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-        },
-      },
-      sections = {
-        { section = "header" },
-        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-        { section = "startup" },
-      },
-    },
+    dashboard = require("plugins.snacks.dashboard"),
     explorer = { enabled = true },
     input = { enabled = true },
     picker = {
@@ -54,11 +21,11 @@ return {
       win = {
         input = {
           keys = {
-            ["<C-f>"] = false,
-            ["<C-b>"] = { "list_scroll_down", mode = { "i", "n" } },
+            ["<C-n>"] = { "list_scroll_down", mode = { "i", "n" } },
+            ["<C-p>"] = { "list_scroll_up", mode = { "i", "n" } },
             ["<C-u>"] = { "preview_scroll_up", mode = { "n" } },
+            ["<C-b>"] = { "preview_scroll_up", mode = { "i" } },
             ["<C-d>"] = { "preview_scroll_down", mode = { "i", "n" } },
-            ["<C-p>"] = { "preview_scroll_up", mode = { "i" } },
           },
         },
       },
