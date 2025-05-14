ef95697b67ecdfaf1dcaa7177307f7185a645206
ymm10ak1 <66200742+ymm10ak1@users.noreply.github.com>
2025-05-14 22:24:22 +0900

    update: MarkdownPreviewができるようにlazy loadとlazy buildを行うキーマップを追加

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -1,10 +1,19 @@
 return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = "markdown",
   keys = {
     { "<M-p>", "<Cmd>MarkdownPreviewToggle<CR>" },
   },
   build = function()
     vim.fn["mkdp#util#install"]()
+  end,
+
+  -- https://github.com/iamcco/markdown-preview.nvim/issues/690#issuecomment-2425301834
+  init = function()
+    vim.keymap.set("n", "<leader>mb", function()
+      vim.cmd([[Lazy load markdown-preview.nvim]])
+      vim.cmd([[Lazy build markdown-preview.nvim]])
+    end, { desc = "markdown-preview.nvimに対してlazy loadとlazy buildを行う" })
   end,
 }
